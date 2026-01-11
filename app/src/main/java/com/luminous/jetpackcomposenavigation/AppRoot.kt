b46380702118dc80a4322b258e0a4bcbbc5f07ed
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-01-11 13:48:19 +0600

    refactoring further

--- app/src/main/java/com/luminous/jetpackcomposenavigation/AppRoot.kt
+++ app/src/main/java/com/luminous/jetpackcomposenavigation/AppRoot.kt
@@ -1,11 +1,13 @@
 package com.luminous.jetpackcomposenavigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.luminous.jetpackcomposenavigation.login.LoginScreen
 import kotlinx.serialization.Serializable
+import kotlin.math.log
 
 @Serializable
 data object LoginDestination
@@ -18,11 +20,21 @@ fun AppRoot() {
                 navController = navController,
                 startDestination = LoginDestination
         ) {
-                composable<LoginDestination> {
-                        LoginScreen(
-                                onNavigateToMain = { },
-                                onNavigateToSignUp = { }
-                        )
-                }
+                loginScreen(
+                        onNavigateToMain = { },
+                        onNavigateToSignUp = { }
+                )
+        }
+}
+
+private  fun NavGraphBuilder.loginScreen(
+        onNavigateToMain: () -> Unit,
+        onNavigateToSignUp: () -> Unit
+) {
+        composable<LoginDestination> {
+                LoginScreen(
+                        onNavigateToMain = onNavigateToMain,
+                        onNavigateToSignUp = onNavigateToSignUp
+                )
         }
 }
