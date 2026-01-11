6e2a68627f36020ecc73cb4fe60fdf990c6fc1ad
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-01-11 11:30:33 +0600

    update gradle

--- app/src/main/java/com/luminous/jetpackcomposenavigation/MainActivity.kt
+++ app/src/main/java/com/luminous/jetpackcomposenavigation/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.luminous.jetpackcomposenavigation.ui.theme.JetPackComposeNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +29,16 @@ class MainActivity : ComponentActivity() {
                         }
                 }
         }
+}
+
+@Composable
+private fun AppRoot() {
+        val navController = rememberNavController()
+
+        NavHost(
+                navController = navController,
+                startDestination = ""
+        )
 }
 
 @Composable
