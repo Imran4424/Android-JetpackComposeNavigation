9e03d6b8cd890bbfe6392fed0d5f5eb17701b61b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-01-12 00:32:41 +0600

    main screen

--- app/src/main/java/com/luminous/jetpackcomposenavigation/main/MainScreen.kt
+++ app/src/main/java/com/luminous/jetpackcomposenavigation/main/MainScreen.kt
@@ -1,13 +1,20 @@
 package com.luminous.jetpackcomposenavigation.main
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.luminous.jetpackcomposenavigation.ui.theme.NavSetupTheme
 
 @Composable
@@ -15,7 +22,25 @@ fun MainScreen(
         onOpenEmailDetails: (emailId: Int) -> Unit,
         onComposeNewEmail: () -> Unit
 ) {
+        val navController = rememberNavController()
 
+        Scaffold(
+                bottomBar = {
+                        MainBottomBar(
+                                hierarchy = navController.currentBackStackEntryAsState().value?.destination?.hierarchy,
+                                onNavigateToEmails = { },
+                                onNavigateToProfile = { }
+                        )
+                }
+        ) { paddingValues ->
+                NavHost(
+                        modifier = Modifier.padding(paddingValues),
+                        navController = navController,
+                        startDestination = ""
+                ) {
+
+                }
+        }
 }
 
 @Composable
