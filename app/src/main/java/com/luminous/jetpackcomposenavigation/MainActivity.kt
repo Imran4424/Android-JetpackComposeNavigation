7c78095e30cac3a2141878cc149d23693addafd6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-01-11 13:17:21 +0600

    add NavSetupTheme

--- app/src/main/java/com/luminous/jetpackcomposenavigation/MainActivity.kt
+++ app/src/main/java/com/luminous/jetpackcomposenavigation/MainActivity.kt
@@ -13,20 +13,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.*
 import com.luminous.jetpackcomposenavigation.ui.theme.JetPackComposeNavigationTheme
+import com.luminous.jetpackcomposenavigation.ui.theme.NavSetupTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
                 enableEdgeToEdge()
                 setContent {
-                        JetPackComposeNavigationTheme {
-                                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                                        Greeting(
-                                                name = "Android",
-                                                modifier = Modifier.padding(innerPadding)
-                                        )
-                                }
+                        NavSetupTheme {
+                                AppRoot()
                         }
                 }
         }
@@ -38,9 +36,11 @@ private fun AppRoot() {
 
         NavHost(
                 navController = navController,
-                startDestination = ""
+                startDestination = LoginDestination
         ) {
+                composable<LoginDestination>(LoginDestination) {
 
+                }
         }
 }
 
@@ -51,6 +51,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 modifier = modifier
         )
 }
+
+@Serializable
+data object LoginDestination
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/luminous/jetpackcomposenavigation/login/LoginScreen.kt
+++ app/src/main/java/com/luminous/jetpackcomposenavigation/login/LoginScreen.kt
@@ -1,2 +1,58 @@
 package com.luminous.jetpackcomposenavigation.login
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.luminous.jetpackcomposenavigation.ui.theme.NavSetupTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(
+        onNavigateToMain: () -> Unit,
+        onNavigateToSignUp: () -> Unit
+) {
+        Scaffold(
+                topBar = {
+                        CenterAlignedTopAppBar(
+                                title = { Text(text = "Login") }
+                        )
+                }
+        ) { padding ->
+                Box(
+                        modifier = Modifier.fillMaxSize().padding(paddingValues = padding),
+                        contentAlignment = Alignment.Center
+                ) {
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                Button(onClick = onNavigateToMain) {
+                                        Text(text = "Login")
+                                }
+
+                                Button(onClick = onNavigateToSignUp) {
+                                        Text(text = "Sign Up")
+                                }
+                        }
+                }
+        }
+}
+
+
+@Preview
+@Composable
+private fun LoginScreenPreview() {
+        NavSetupTheme {
+                LoginScreen(
+                        onNavigateToMain = {},
+                        onNavigateToSignUp = {}
+                )
+        }
+}
