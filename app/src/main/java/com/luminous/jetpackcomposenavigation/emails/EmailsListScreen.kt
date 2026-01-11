4c53cb2aac129b66e72bb83f4fdc9881b811dffa
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-01-11 17:26:36 +0600

    emails list

--- app/src/main/java/com/luminous/jetpackcomposenavigation/emails/EmailsListScreen.kt
+++ app/src/main/java/com/luminous/jetpackcomposenavigation/emails/EmailsListScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
@@ -24,8 +25,14 @@ fun EmailsListScreen(
         Scaffold(
                 modifier = Modifier.fillMaxSize(),
                 contentWindowInsets = WindowInsets(0.dp),
-                topBar = {}
-
+                topBar = { EmailsSearchBar() },
+                floatingActionButton = {
+                        ExtendedFloatingActionButton(
+                                text = { Text(text = "Compose") },
+                                onClick = onComposeNewEmail,
+                                icon = { Icon(imageVector = Icons.Default.Search, null) }
+                        )
+                }
         ) { }
 }
 
