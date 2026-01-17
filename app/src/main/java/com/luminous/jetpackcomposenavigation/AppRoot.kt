edafc54c784fcd43f5e8191f7af70effd8d6f259
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-01-17 19:43:03 +0600

    composer

--- app/src/main/java/com/luminous/jetpackcomposenavigation/AppRoot.kt
+++ app/src/main/java/com/luminous/jetpackcomposenavigation/AppRoot.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.luminous.jetpackcomposenavigation.emails.navigateToEmailDetails
 import com.luminous.jetpackcomposenavigation.login.LoginDestination
 import com.luminous.jetpackcomposenavigation.login.LoginScreen
 import com.luminous.jetpackcomposenavigation.login.loginScreen
