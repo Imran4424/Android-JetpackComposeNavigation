7f832400daf4a45002c0dac6650f8171f5b35e1b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-01-17 20:04:35 +0600

    composer

--- app/src/main/java/com/luminous/jetpackcomposenavigation/composer/ComposerSettingsNavigation.kt
+++ app/src/main/java/com/luminous/jetpackcomposenavigation/composer/ComposerSettingsNavigation.kt
@@ -1,2 +1,23 @@
 package com.luminous.jetpackcomposenavigation.composer
 
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
+
+@Serializable
+private data object ComposerSettingsDestination
+
+fun NavGraphBuilder.composerSettingsScreen(
+        onNavigateUp: () -> Unit
+) {
+        composable<ComposerSettingsDestination> {
+                ComposerSettingsScreen(
+                        onNavigateUp = onNavigateUp
+                )
+        }
+}
+
+fun NavController.navigateToComposerSettings() {
+        navigate(ComposerSettingsDestination)
+}
