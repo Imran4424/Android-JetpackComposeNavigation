570c2783b766e0c3693a426765d0a57783bc5edc
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-01-17 20:00:53 +0600

    composer

--- app/src/main/java/com/luminous/jetpackcomposenavigation/composer/ComposerScreenRoot.kt
+++ app/src/main/java/com/luminous/jetpackcomposenavigation/composer/ComposerScreenRoot.kt
@@ -3,13 +3,32 @@ package com.luminous.jetpackcomposenavigation.composer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposableOpenTarget
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.luminous.jetpackcomposenavigation.ui.theme.NavSetupTheme
+import kotlinx.serialization.Serializable
+
+@Serializable
+private data object ComposerDestination
 
 @Composable
 fun ComposerScreenRoot(
         onNavigateUp: () -> Unit
 ) {
+        val navController = rememberNavController()
 
+        NavHost(
+                navController = navController,
+                startDestination = ComposerDestination
+        ) {
+                composable<ComposerDestination> {
+                        ComputerScreen(
+                                onNavigateUp = onNavigateUp,
+                                onOpenSettings = { navController.navigateToComposerSettings() }
+                        )
+                }
+        }
 }
 
 @Preview
